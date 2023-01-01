8ea20251878f3154b59dd794cc792ef5aa6eccef
Takao Akaki <mongonta@gmail.com>
2023-01-01 15:19:26 +0900

    1st commit

---
+++ src/fft.hpp
@@ -0,0 +1,93 @@
+#include <Arduino.h>
+// This source is extracted from the following repository source.(examples/Advanced/Bluetooth_with_ESP32A2DP)
+// https://github.com/m5stack/M5Unified
+
+#define FFT_SIZE 256
+class fft_t
+{
+  float _wr[FFT_SIZE + 1];
+  float _wi[FFT_SIZE + 1];
+  float _fr[FFT_SIZE + 1];
+  float _fi[FFT_SIZE + 1];
+  uint16_t _br[FFT_SIZE + 1];
+  size_t _ie;
+
+public:
+  fft_t(void)
+  {
+#ifndef M_PI
+#define M_PI 3.141592653
+#endif
+    _ie = logf( (float)FFT_SIZE ) / log(2.0) + 0.5;
+    static constexpr float omega = 2.0f * M_PI / FFT_SIZE;
+    static constexpr int s4 = FFT_SIZE / 4;
+    static constexpr int s2 = FFT_SIZE / 2;
+    for ( int i = 1 ; i < s4 ; ++i)
+    {
+    float f = cosf(omega * i);
+      _wi[s4 + i] = f;
+      _wi[s4 - i] = f;
+      _wr[     i] = f;
+      _wr[s2 - i] = -f;
+    }
+    _wi[s4] = _wr[0] = 1;
+
+    size_t je = 1;
+    _br[0] = 0;
+    _br[1] = FFT_SIZE / 2;
+    for ( size_t i = 0 ; i < _ie - 1 ; ++i )
+    {
+      _br[ je << 1 ] = _br[ je ] >> 1;
+      je = je << 1;
+      for ( size_t j = 1 ; j < je ; ++j )
+      {
+        _br[je + j] = _br[je] + _br[j];
+      }
+    }
+  }
+
+  void exec(const int16_t* in)
+  {
+    memset(_fi, 0, sizeof(_fi));
+    for ( size_t j = 0 ; j < FFT_SIZE / 2 ; ++j )
+    {
+      float basej = 0.25 * (1.0-_wr[j]);
+      size_t r = FFT_SIZE - j - 1;
+
+      /// perform han window and stereo to mono convert.
+      _fr[_br[j]] = basej * (in[j * 2] + in[j * 2 + 1]);
+      _fr[_br[r]] = basej * (in[r * 2] + in[r * 2 + 1]);
+    }
+
+    size_t s = 1;
+    size_t i = 0;
+    do
+    {
+      size_t ke = s;
+      s <<= 1;
+      size_t je = FFT_SIZE / s;
+      size_t j = 0;
+      do
+      {
+        size_t k = 0;
+        do
+        {
+          size_t l = s * j + k;
+          size_t m = ke * (2 * j + 1) + k;
+          size_t p = je * k;
+          float Wxmr = _fr[m] * _wr[p] + _fi[m] * _wi[p];
+          float Wxmi = _fi[m] * _wr[p] - _fr[m] * _wi[p];
+          _fr[m] = _fr[l] - Wxmr;
+          _fi[m] = _fi[l] - Wxmi;
+          _fr[l] += Wxmr;
+          _fi[l] += Wxmi;
+        } while ( ++k < ke) ;
+      } while ( ++j < je );
+    } while ( ++i < _ie );
+  }
+
+  uint32_t get(size_t index)
+  {
+    return (index < FFT_SIZE / 2) ? (uint32_t)sqrtf(_fr[ index ] * _fr[ index ] + _fi[ index ] * _fi[ index ]) : 0u;
+  }
+};
---
+++ src/main.cpp
@@ -0,0 +1,216 @@
+#include <M5Unified.h>
+#include <Avatar.h>
+#include "fft.hpp"
+
+#define USE_MIC
+
+#ifdef USE_MIC
+  // ---------- Mic sampling ----------
+
+  int pin_clk  = -1;
+  int pin_data = -1;
+  //#define PIN_CLK     0
+  //#define PIN_DATA    34
+  #define READ_LEN    (2 * 256)
+  #define GAIN_FACTOR 3
+  uint8_t BUFFER[READ_LEN] = {0};
+
+  int16_t *adcBuffer = NULL;
+  static fft_t fft;
+  static constexpr size_t WAVE_SIZE = 320;
+  static int16_t raw_data[WAVE_SIZE * 2];
+
+  float lipsync_max = 300.0f;  // リップシンクの単位ここを増減すると口の開き方が変わります。
+
+#endif
+
+
+
+
+using namespace m5avatar;
+
+Avatar avatar;
+ColorPalette *cps[6];
+uint8_t palette_index = 0;
+
+uint32_t last_rotation_msec = 0;
+
+void initI2S()  // Init I2S.  初始化I2S
+{
+    i2s_config_t i2s_config = {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX |
+                             I2S_MODE_PDM),  // Set the I2S operating mode.
+                                             // 设置I2S工作模式
+        .sample_rate = 44100,  // Set the I2S sampling rate.  设置I2S采样率
+        .bits_per_sample =
+            I2S_BITS_PER_SAMPLE_16BIT,  // Fixed 12-bit stereo MSB.
+                                        // 固定为12位立体声MSB
+        .channel_format =
+            I2S_CHANNEL_FMT_ALL_RIGHT,  // Set the channel format.  设置频道格式
+#if ESP_IDF_VERSION > ESP_IDF_VERSION_VAL(4, 1, 0)
+        .communication_format =
+            I2S_COMM_FORMAT_STAND_I2S,  // Set the format of the communication.
+#else                                   // 设置通讯格式
+        .communication_format = I2S_COMM_FORMAT_I2S,
+#endif
+        .intr_alloc_flags =
+            ESP_INTR_FLAG_LEVEL1,  // Set the interrupt flag.  设置中断的标志
+        .dma_buf_count = 2,        // DMA buffer count.  DMA缓冲区计数
+        .dma_buf_len   = 128,      // DMA buffer length.  DMA缓冲区长度
+    };
+
+    i2s_pin_config_t pin_config;
+
+#if (ESP_IDF_VERSION > ESP_IDF_VERSION_VAL(4, 3, 0))
+    pin_config.mck_io_num = I2S_PIN_NO_CHANGE;
+#endif
+
+    pin_config.bck_io_num   = I2S_PIN_NO_CHANGE;
+    pin_config.ws_io_num    = pin_clk;
+    pin_config.data_out_num = I2S_PIN_NO_CHANGE;
+    pin_config.data_in_num  = pin_data;
+
+    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_NUM_0, &pin_config);
+    i2s_set_clk(I2S_NUM_0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
+}
+
+int8_t i2s_readraw_buff[1024];
+uint8_t fft_dis_buff[161][80] = {0};
+uint16_t posData              = 160;
+
+void lipsync() {
+  
+  size_t bytesread;
+  uint64_t level = 0;
+  i2s_read(I2S_NUM_0, (char *)i2s_readraw_buff, 1024, &bytesread,
+              (100 / portTICK_RATE_MS));
+  adcBuffer = (int16_t *)i2s_readraw_buff;
+  fft.exec(adcBuffer);
+  for (size_t bx=5;bx<=60;++bx) {
+    int32_t f = fft.get(bx);
+    level += abs(f);
+  }
+
+  Serial.printf("level:%d\n", level) ;         // lipsync_maxを調整するときはこの行をコメントアウトしてください。
+  float ratio = (float)((level >> 9)/lipsync_max);
+  // Serial.printf("ratio:%f\n", ratio);
+  if (ratio <= 0.01f) {
+    ratio = 0.0f;
+  } else if (ratio >= 1.3f) {
+    ratio = 1.3f;
+  }
+  if ((millis() - last_rotation_msec) > 350) {
+    int direction = random(2);
+    if (direction == 1) {
+      avatar.setRotation(20 * ratio);
+    } else {
+      avatar.setRotation(-20 * ratio);
+    }
+    last_rotation_msec = millis();
+  }
+  avatar.setMouthOpenRatio(ratio);
+}
+
+
+void setup()
+{
+  auto cfg = M5.config();
+  M5.begin(cfg);
+  float scale = 0.0f;
+  int8_t position_x = 0;
+  int8_t position_y = 0;
+  uint8_t display_rotation = 3; // ディスプレイの向き(0〜3)
+  uint8_t first_cps = 0;
+  switch (M5.getBoard()) {
+    case m5::board_t::board_M5AtomS3:
+      first_cps = 4;
+      scale = 0.5f;
+      position_x = 5;
+      position_y = -15;
+      pin_clk  = 1;
+      pin_data = 2;
+      break;
+
+    case m5::board_t::board_M5StickC:
+      first_cps = 1;
+      scale = 0.6f;
+      position_x = -30;
+      position_y = -15;
+      pin_clk  = 0;
+      pin_data = 34;
+      break;
+
+    case m5::board_t::board_M5StickCPlus:
+      first_cps = 2;
+      scale = 0.7f;
+      position_x = -15;
+      position_y = 5;
+      pin_clk  = 0;
+      pin_data = 34;
+      break;
+    
+    case m5::board_t::board_M5Stack:
+      scale = 1.0f;
+      position_x = 0;
+      position_y = 0;
+      display_rotation = 1;
+      break;
+
+    defalut:
+      Serial.println("Invalid board.");
+      break;
+  }
+  initI2S();
+  M5.Lcd.setRotation(display_rotation);
+  avatar.setScale(scale);
+  avatar.setPosition(position_x, position_y);
+  avatar.init(); // start drawing
+  cps[0] = new ColorPalette();
+  cps[0]->set(COLOR_PRIMARY, TFT_BLACK);
+  cps[0]->set(COLOR_BACKGROUND, TFT_YELLOW);
+  cps[1] = new ColorPalette();
+  cps[1]->set(COLOR_PRIMARY, TFT_BLACK);
+  cps[1]->set(COLOR_BACKGROUND, TFT_ORANGE);
+  cps[2] = new ColorPalette();
+  cps[2]->set(COLOR_PRIMARY, (uint16_t)0x00ff00);
+  cps[2]->set(COLOR_BACKGROUND, (uint16_t)0x303303);
+  cps[3] = new ColorPalette();
+  cps[3]->set(COLOR_PRIMARY, TFT_WHITE);
+  cps[3]->set(COLOR_BACKGROUND, TFT_BLACK);
+  cps[4] = new ColorPalette();
+  cps[4]->set(COLOR_PRIMARY, TFT_BLACK);
+  cps[4]->set(COLOR_BACKGROUND, TFT_WHITE);
+  cps[5] = new ColorPalette();
+  cps[5]->set(COLOR_PRIMARY, 0x303303);
+  cps[5]->set(COLOR_BACKGROUND, 0x00ff00);
+  avatar.setColorPalette(*cps[first_cps]);
+  //avatar.addTask(lipsync, "lipsync");
+  last_rotation_msec = millis();
+}
+
+uint32_t count = 0;
+
+void loop()
+{
+  M5.update();
+  if (M5.BtnA.wasPressed()) {
+    palette_index++;
+    if (palette_index > 5) {
+      palette_index = 0;
+    }
+    avatar.setColorPalette(*cps[palette_index]);
+  }
+  
+//  if ((millis() - last_rotation_msec) > 100) {
+    //float angle = 10 * sin(count);
+    //avatar.setRotation(angle);
+    //last_rotation_msec = millis();
+    //count++;
+  //}
+
+  // avatar's face updates in another thread
+  // so no need to loop-by-loop rendering
+  lipsync();
+  vTaskDelay(1/portTICK_PERIOD_MS);
+}
